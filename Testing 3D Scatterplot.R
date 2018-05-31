9c06ef7c5181af58b523c9432b9fb56ec73a042f
Luna Liu <liuluna2001@gmail.com>
2018-05-31 09:45:21 -0400

    commit

--- Testing 3D Scatterplot.R
+++ Testing 3D Scatterplot.R
@@ -3,5 +3,10 @@ library("scatterplot3d")
 path<-file.choose()
 dattbl<-(read.csv(path)
 )
-View(dattbl)
-scatterplot3d(dattbl[,2:4])
+#View(dattbl)
+scatterplot3d(dattbl[,2:4], 
+              xlab="Cancer Onset", 
+              ylab="Probablility of Developing Cancer",
+              zlab="Risk",
+              main="Probability of Developing Cancer for each age group",
+              angle=75)
