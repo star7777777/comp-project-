30d6ea6144db762800412fd2974cade44649c1a4
Luna Liu <liuluna2001@gmail.com>
2018-06-01 22:52:49 -0400

    This should be correct now

--- Testing Histogram.R
+++ Testing Histogram.R
@@ -2,14 +2,14 @@
 #Generally, Histograms are used for showing how much data each individual independent value will have.
 #Please replace any and all instances of Dattbl before turning in the code. Thx :)
 
-#path<-file.choose()
-#dattbl<-read.csv(path)
-hit<-hist(dattbl$ Rate,
-     main="Histogram of Survival Time",
-     xlab="Survival Time (Years)",
+path<-file.choose()
+dattbl<-read.csv(path)
+hit<-hist(dattbl$ Risk,
+     main="Histogram of Probablility of Dying From Cancer",
+     xlab="Probablility of Dying From Cancer",
      col="red")
 #Creates a density plot for the data table.
-plot(density(dattbl$Rate),
-     main="Density Plot of Survival Time",
-     xlab="Survival Time (Years)")
+plot(density(dattbl$Risk, na.rm=TRUE),
+     main="Density Plot of Probablility of Dying From Cancer",
+     xlab="Probablility of Dying From Cancer")
      
